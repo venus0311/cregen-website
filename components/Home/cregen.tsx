cb2042f0129598a27a00c23c4d66d67fe5a1ebd2
top <fox0905@outlook.com>
2025-07-01 18:47:10 -1200

    feat: home first section

--- components/Home/cregen.tsx
+++ components/Home/cregen.tsx
@@ -5,14 +5,25 @@ import Image from "next/image";
 
 export default function Cregen() {
   return (
-    <div className="pt-32 pb-[270px]">
-      <div className="flex flex-col items-start justify-center text-white font-Neue pt-40 pl-[100px] gap-10 pr-10">
-        <p className="text-[112px] font-thin font-Neue leading-none uppercase tracking-[0%]">CREGEN</p>
-        <p className="text-[112px] font-extrabold opacity-10 leading-none uppercase animate-fadein [animation-delay:.5s]">DRIVEN</p>
-        <p className="text-[112px] font-extrabold opacity-10 leading-none uppercase animate-fadein [animation-delay:.9s]">BY CURIOSITY</p>
+    <div className="relative pt-32 pb-[270px]">
+      <video
+        autoPlay
+        loop
+        muted
+        playsInline
+        className="absolute inset-0 w-full h-full object-cover"
+      >
+        <source src="./videos/cregenweback.mp4" type="video/mp4" />
+      </video>
+      <div className="relative flex flex-col items-start justify-center text-white font-Neue sm:pt-40 pl-10 sm:pl-[100px] gap-6 sm:gap-10 pr-10">
+        <p className="text-[40px] lg:text-[112px] font-thin font-Neue leading-none uppercase tracking-[0%]">CREGEN</p>
+        <p className="text-[40px] lg:text-[112px] font-extrabold opacity-10 leading-none uppercase animate-fadein [animation-delay:.5s]">DRIVEN</p>
+        <p className="text-[40px] lg:text-[112px] font-extrabold opacity-10 leading-none uppercase animate-fadein [animation-delay:.9s]">BY CURIOSITY</p>
         <div className="flex items-center gap-10 text-white font-Neue uppercase">
           <p className=" text-sm font-light">The Story</p>
-          <TheStory />
+          <div className="hidden sm:block sm:w-full h-full">
+            <TheStory />
+          </div>
         </div>
         <Link href="/contactUs" className="flex w-full items-center justify-end text-white font-Neue cursor-pointer gap-2">
           <p className=" text-sm font-light">Get in Touch</p>
